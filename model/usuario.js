6035323dfaddb2420a6fb75f5f5fd9d2e98def51
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-05-17 15:44:42 -0400

    C-143 Autenticacion JWT

--- model/usuario.js
+++ model/usuario.js
@@ -1,44 +1,43 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UsuarioSchema = Schema({
-
-    nombre: {
-        type: String,
-        required: [true, 'El nombre es obligatorio']
-    },
-    correo: {
-        type: String,
-        required: [true, 'El correo es obligatorio'],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, 'El contraseña es obligatoria'],
-        unique: true
-    },
-    img: {
-        type: String,
-    },
-    rol: {
-        type: String,
-        required: true,
-        emun: ['ADMIN_ROLE', 'USER_ROLE']
-    },
-    estado: {
-        type: Boolean,
-        default: true
-    },
-    google: {
-        type: Boolean,
-        default: false
-    }
+  nombre: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  correo: {
+    type: String,
+    required: [true, "El correo es obligatorio"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "El contraseña es obligatoria"],
+    unique: true,
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    emun: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-
 // Modificando el Modelo
-UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, ...usuario } = this.toObject();
-    return usuario;
+UsuarioSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...usuario } = this.toObject();
+  usuario.uid = _id;
+  return usuario;
 };
 
-module.exports = model('Usuario', UsuarioSchema);
+module.exports = model("Usuario", UsuarioSchema);
