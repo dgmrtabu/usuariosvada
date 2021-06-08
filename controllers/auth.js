5a03ae0c38f9e36091836ec0837b067351dd5d35
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-06-08 12:15:56 -0400

    C-159 Google SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,42 +3,43 @@ const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../model/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+const usuario = require("../model/usuario");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
 
   try {
-        //Verificar si el email existe
-        const usuario = await Usuario.findOne({ correo });
-        if (!usuario) {
-          return res.status(400).json({
-            msg: "Usuario / Password no son correctos",
-          });
-        }
-        //Si el usuario esta actiVo
-        if (!usuario.estado) {
-          return res.status(400).json({
-            msg: "Estado inactivos",
-          });
-        }
+    //Verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos",
+      });
+    }
+    //Si el usuario esta actiVo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Estado inactivos",
+      });
+    }
 
-        // Verificar la contraseña
+    // Verificar la contraseña
 
-        const validPassword = bcryptjs.compareSync(password, usuario.password);
-        if (!validPassword) {
-          return res.status(400).json({
-            msg: "Contraseña incorrecta",
-          });
-        }
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Contraseña incorrecta",
+      });
+    }
 
-        //  Generar el JWT
-        const token = await generarJWT(usuario.id);
+    //  Generar el JWT
+    const token = await generarJWT(usuario.id);
 
-        res.json({
-          usuario,
-          token,
-        });
-
+    res.json({
+      usuario,
+      token,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -46,6 +47,49 @@ const login = async (req, res = response) => {
     });
   }
 };
+
+const googleSignin = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, correo, img } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      const data = {
+        // Crear usuario
+        nombre,
+        correo,
+        password: ":P",
+        img,
+        google: true,
+      };
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+    // Si el usuario en DB
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el administrador del sistema",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Token de Goolge no es valido",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignin,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignin } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
+
 const router = Router();
+
 router.post('/login', [
     check('correo', 'El correo es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/google', [
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignin);
 
 module.exports = router;
