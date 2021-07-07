d1894014a6745f54466cc8084322a8a1bb42a610
dgmr.tabu <dgmr.tabu@gmail.com>
2021-07-06 20:38:26 -0400

    C-176 Categorias y productos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,13 +1,64 @@
 const { request, response } = require("express");
+const { ObjectId } = require('mongoose').Types;
+
+const { Usuario, Categoria, Producto } = require('../model');
+const coleccionesPermitidas = [
+    'usuarios',
+    'categorias',
+    'productos',
+    'roles',
+];
+
+const buscarUsuarios = async(termino = '', res = response) => {
+
+    const esMongoId = ObjectId.isValid(termino); // Retorna u TRUE
+
+    if (esMongoId) {
+        const usuario = await Usuario.findById(termino);
+        return res.json({
+            results: (usuario) ? [usuario] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const usuarios = await Usuario.find({
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+    });
+
+    res.json({
+        results: usuarios
+    });
+
+};
+
 
 const buscar = (req = request, res = response) => {
 
     const { coleccion, termino } = req.params;
 
-    res.json({
-        coleccion,
-        termino
-    });
+    if (!coleccionesPermitidas.includes(coleccion)) {
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${ coleccionesPermitidas }`
+        });
+    }
+
+    switch (coleccion) {
+        case 'usuarios':
+            buscarUsuarios(termino, res);
+            break;
+        case 'categorias':
+            break;
+        case 'productos':
+            break;
+
+        default:
+            res.status(500).json({
+                msg: 'Se me olvido hacer esta busqueda'
+            });
+            break;
+    }
 };
 
 module.exports = {
