1628e5ffa60ee6ed64ccde73f82529a086be5de6
dgmr.tabu <dgmr.tabu@gmail.com>
2021-07-12 21:41:06 -0400

    C-181 Carga de Archivos

--- database/config.js
+++ database/config.js
@@ -20,4 +20,4 @@ const dbConnection = async() => {
 
 module.exports = {
     dbConnection
-}
+};
--- model/server.js
+++ model/server.js
@@ -14,6 +14,7 @@ class Server {
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios: '/api/usuarios',
+            uploads: '/api/uploads',
         };
 
         // Conectando a BD
@@ -50,6 +51,7 @@ class Server {
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
 
     }
 
