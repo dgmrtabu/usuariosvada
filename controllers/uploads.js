ea2468a3188062444ba4c453bbf37dd55f62417a
dgmr.tabu <dgmr.tabu@gmail.com>
2021-07-28 21:12:23 -0400

    C-192 Carga de Archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -69,7 +69,54 @@ const actualizarImagen = async(req = request, res = response) => {
     res.json(modelo);
 };
 
+const mostarImagen = async(req, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch ((coleccion)) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el ID ${id}`
+                });
+            }
+
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el ID ${id}`
+                });
+            }
+
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvido validar esto' });
+            break;
+    }
+
+
+    // Borrar imagenes previas
+    if (modelo.img) {
+        // hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen);
+        }
+    }
+
+    const pathImagen = path.join(__dirname, '../assets/no-image.png');
+    res.sendFile(pathImagen);
+
+}
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostarImagen
 };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,19 +1,27 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
 
 
 const router = Router();
 
+router.get('/:coleccion/:id', [
+    check('id', 'El Id debe ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostarImagen)
+
 router.post('/', validarArchivoSubir, cargarArchivo);
+
 router.put('/:coleccion/:id', [
     validarArchivoSubir,
     check('id', 'El Id debe ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
 ], actualizarImagen);
+
 
 
 module.exports = router;
